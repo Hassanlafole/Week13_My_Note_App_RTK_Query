4b036f755bf60a899865a6a2b3d457b5f4191151
user two <xasannuur3344@gmail.com>
2023-06-24 13:44:54 -0700

    rtk

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import {noteSlice} from './api/NoteSlice';
+import {setupListeners} from '@reduxjs/toolkit/dist/query'
+
+export const store = configureStore({
+  reducer: {
+    [noteSlice.reducerPath]: noteSlice.reducer
+    },
+
+    // middleware
+    middleware : (getDefaultMiddleware)  => getDefaultMiddleware().concat(noteSlice.middleware)
+});
+
+setupListeners(store.dispatch)
